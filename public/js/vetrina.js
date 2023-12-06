1ae98a7a8da6fc1e163b5197e5a046b22298bb41
Davide Battaglia <davide.battaglia-1@studenti.unitn.it>
2023-12-06 14:40:10 +0100

    Delete

--- public/js/vetrina.js
+++ public/js/vetrina.js
@@ -34,6 +34,51 @@ if (token) {
 }
 
 function renderShoes(shoesData) {
+    const shoeTableContainer = document.getElementById('shoeTable');
+
+    if (shoesData && Array.isArray(shoesData)) {
+        const shoeTable = document.createElement('table');
+        shoeTable.classList.add('shoe-table'); // Aggiungi una classe per lo stile (CSS)
+
+        // Intestazione della tabella
+        const tableHeader = document.createElement('thead');
+        const headerRow = document.createElement('tr');
+        const headerColumns = ['Marca', 'Modello', 'Descrizione', 'Prezzo'];
+
+        headerColumns.forEach(columnText => {
+            const headerCell = document.createElement('th');
+            headerCell.textContent = columnText;
+            headerRow.appendChild(headerCell);
+        });
+
+        tableHeader.appendChild(headerRow);
+        shoeTable.appendChild(tableHeader);
+
+        // Corpo della tabella
+        const tableBody = document.createElement('tbody');
+
+        shoesData.forEach(shoe => {
+            const shoeRow = document.createElement('tr');
+
+            ['brand', 'model', 'description', 'price'].forEach(column => {
+                const cell = document.createElement('td');
+                cell.textContent = shoe[column];
+                shoeRow.appendChild(cell);
+            });
+
+            tableBody.appendChild(shoeRow);
+        });
+
+        shoeTable.appendChild(tableBody);
+        shoeTableContainer.innerHTML = ''; // Pulisci il contenitore prima di aggiungere la nuova tabella
+        shoeTableContainer.appendChild(shoeTable);
+    } else {
+        console.error('Dati sulle scarpe non validi.');
+    }
+}
+
+/*
+function renderShoes(shoesData) {
     // Esempio di come potresti utilizzare i dati ricevuti
     // Ad esempio, puoi creare elementi HTML e aggiungerli al DOM
     const shoeListContainer = document.getElementById('shoeList');
@@ -53,3 +98,5 @@ function renderShoes(shoesData) {
         console.error('Dati sulle scarpe non validi.');
     }
 }
+
+*/
