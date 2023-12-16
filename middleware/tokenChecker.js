e6db12ee2f1357d3f832031d382deff285dc4238
Davide Battaglia <davide.battaglia-1@studenti.unitn.it>
2023-12-16 12:21:03 +0100

    Test jest

--- middleware/tokenChecker.js
+++ middleware/tokenChecker.js
@@ -1,45 +1,45 @@
-const jwt = require('jsonwebtoken'); // used to create, sign, and verify tokens
-const dotenv = require('dotenv');
-dotenv.config();
-const secretKey = process.env.SECRET_KEY || 'defaultSecretKey';
+const request = require('supertest');
+const app = require('../app');
+const User = require('../models/user');
+const jwt = require('jsonwebtoken');
 
-const tokenChecker = function(req, res, next) {
-	console.log("Chiave segreta:", process.env.SECRET_KEY);
+describe('Test della rotta /login con tokenChecker', () => {
+  // Pulisce il database prima di ogni test
+  beforeEach(async () => {
+    await User.deleteMany();
+  });
 
-	// check header or url parameters or post parameters for token
-	//console.log("Before token assignment:", req.body.token, req.query.token, req.headers['x-access-token'], req.headers['authorization']);
-	var token = req.headers['authorization'] || req.headers['x-access-token'] || req.query.token || req.body.token;
-	console.log("tokenChecherTOP_token: ", token);
-	// if there is no token
-	if (!token) {
-		return res.status(401).send({ 
-			success: false,
-			message: 'No token provided.'
-		});
-	}
+  it('Dovrebbe autenticare l\'utente, restituire uno stato 200 e ricevere un token valido', async () => {
+    // Crea un utente nel database
+    const hashedPassword = await bcrypt.hash('password_sicura', 10);
+    await User.create({
+      username: 'utente_autenticato',
+      password: hashedPassword,
+    });
 
-	// decode token, verifies secret and checks exp
-	jwt.verify(token, process.env.SECRET_KEY, function(err, decoded) {			
-		if (err) {
-			if (err.name === 'TokenExpiredError') {
-				console.log('Token scaduto');
-			} else if (err.name === 'JsonWebTokenError') {
-				console.log('Errore di JWT:', err.message);
-			} else {
-				console.log('Altro tipo di errore durante la verifica del token:', err.message);
-			}
-			return res.status(403).send({
-				success: false,
-				message: 'Failed to authenticate token.'
-			});		
-		} else {
-			// if everything is good, save to request for use in other routes
-			console.log("[tokenChecer.ejs]everything is good");
-			req.loggedUser = decoded;
-			next();
-		}
-	});
-	
-};
+    const credentials = {
+      username: 'utente_autenticato',
+      password: 'password_sicura',
+    };
 
-module.exports = tokenChecker
+    // Effettua l'accesso e ricevi il token
+    const loginResponse = await request(app)
+      .post('/login')
+      .send(credentials);
+
+    expect(loginResponse.status).toBe(200);
+    expect(loginResponse.body).toHaveProperty('token');
+
+    const token = loginResponse.body.token;
+
+    // Usa il token per accedere a una rotta protetta
+    const protectedRouteResponse = await request(app)
+      .get('/protected')
+      .set('Authorization', `${token}`);
+
+    // Verifica che la rotta protetta ritorni uno stato 200
+    expect(protectedRouteResponse.status).toBe(200);
+  });
+
+  // Aggiungi ulteriori test se necessario
+});
